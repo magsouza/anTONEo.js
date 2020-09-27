ff15d2dee5e8132028f5e0dba5161b7efd247902
Magnon Souza <mprs@cin.ufpe.br>
2020-09-27 14:51:08 -0300

    initial tests with tone

---
+++ script.js
@@ -0,0 +1,20 @@
+import * as Tone from 'tone'
+import 'regenerator-runtime/runtime'
+
+// const sampler = new Sampler(
+//   {
+//     A1: "A1.mp3"
+//   },
+//   {
+//     onload: () => {
+//       document.querySelector("button").removeAttribute("disabled");
+//     }
+//   }
+// )
+
+document.querySelector("button").addEventListener("click", async () => {
+    console.log('oi')
+    await Tone.start()
+    const osc = new Tone.FMOscillator("Bb4").toDestination().start()
+    osc.frequency.rampTo("D2", 3)
+})
