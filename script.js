bf07f8305d99bbdd0744ce53876fca3c66d8ed70
Magnon Souza <mprs@cin.ufpe.br>
2020-09-28 22:45:47 -0300

    play song function implemented (not functional yet)

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import * as Tone from 'tone'
 import 'regenerator-runtime/runtime'
 import Options from './options'
+import songs from './songs'
+
+Tone.Transport.bpm.value = 100
 
 const pulseSynth    = new Tone.Synth(Options.pulseOptions)
 const squareSynth   = new Tone.Synth(Options.squareOptions)
@@ -15,8 +18,75 @@ let noisePart    = new Tone.Part()
 
 const button = document.querySelector("button")
 
+function setSong() {
+
+    song = songs[0]
+
+    pulsePart.removeAll()
+    squarePart.removeAll()
+    trianglePart.removeAll()
+    noisePart.removeAll()
+
+    if (song.pulse != null) {
+        pulsePart = new Tone.Part((time, note) => {
+            pulseSynth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
+        }, song.pulse)
+      }
+      
+      if(song.square != null) {
+        squarePart = new Tone.Part((time, note) => {
+            squareSynth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
+        }, song.square)
+      }
+      
+      if(song.triangle != null) {
+        trianglePart = new Tone.Part((time, note) => {
+            triangleSynth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
+        }, song.triangle)
+      }
+      
+      if(song.noise != null) {
+        noisePart = new Tone.Part((time, note) => {
+            noiseSynth.triggerAttackRelease(note.duration, time, note.velocity)
+        }, song.noise)
+      }
+}
+
+function start() {
+    if(song.pulse != null)    pulsePart.start(0)
+    if(song.square != null)   squarePart.start(0)
+    if(song.triangle != null) trianglePart.start(0)
+    if(song.noise != null)    noisePart.start(0)
+}
+
+function stop() {
+    if(song.pulse != null)    pulsePart.stop(0)
+    if(song.square != null)   squarePart.stop(0)
+    if(song.triangle != null) trianglePart.stop(0)
+    if(song.noise != null)    noisePart.stop(0)
+}
+
+setSong()
+
+button.addEventListener('click', () => {
+    if (Tone.Transport.state == 'started') {
+        Tone.Transport.stop()
+        button.textContent = 'Restart'
+        stop()
+    } else {
+        Tone.Transport.start('+0.1', 0)
+        start()
+        Tone.Transport.stop('+' + song.length)
+        button.textContent = 'Stop'
+    }
+})
+
+Tone.Transport.on('stop', () => { button.textContent = 'Restart' })
+
+/*
 button.addEventListener("click", async () => {
     await Tone.start()
     const osc = new Tone.FMOscillator("Bb4").toDestination().start()
     osc.frequency.rampTo("D2", 3)
 })
+*/
