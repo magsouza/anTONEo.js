5ea778adf61a56c6d41c758af524b5d75230e36d
Magnon Souza <mprs@cin.ufpe.br>
2020-09-28 21:35:07 -0300

    created synths and tone events (tone.part)

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 import * as Tone from 'tone'
 import 'regenerator-runtime/runtime'
+import Options from './options'
 
-// const sampler = new Sampler(
-//   {
-//     A1: "A1.mp3"
-//   },
-//   {
-//     onload: () => {
-//       document.querySelector("button").removeAttribute("disabled");
-//     }
-//   }
-// )
+const pulseSynth    = new Tone.Synth(Options.pulseOptions)
+const squareSynth   = new Tone.Synth(Options.squareOptions)
+const triangleSynth = new Tone.Synth(Options.triangleOptions)
+const noiseSynth    = new Tone.NoiseSynth()
 
-document.querySelector("button").addEventListener("click", async () => {
+let song = {}
+let pulsePart    = new Tone.Part()
+let squarePart   = new Tone.Part()
+let trianglePart = new Tone.Part()
+let noisePart    = new Tone.Part()
+
+const button = document.querySelector("button")
+
+button.addEventListener("click", async () => {
     await Tone.start()
     const osc = new Tone.FMOscillator("Bb4").toDestination().start()
     osc.frequency.rampTo("D2", 3)
