91c259158441173c1f9e1175d8294f793e30e656
Magnon Souza <mprs@cin.ufpe.br>
2020-09-29 21:17:23 -0300

    synths and tone parts and time fixed

--- script.js
+++ script.js
@@ -5,49 +5,49 @@ import songs from './songs'
 
 Tone.Transport.bpm.value = 100
 
-const pulseSynth    = new Tone.Synth(Options.pulseOptions)
-const squareSynth   = new Tone.Synth(Options.squareOptions)
-const triangleSynth = new Tone.Synth(Options.triangleOptions)
-const noiseSynth    = new Tone.NoiseSynth()
+const pulseSynth    = new Tone.Synth(Options.pulseOptions).toDestination()
+const squareSynth   = new Tone.Synth(Options.squareOptions).toDestination()
+const triangleSynth = new Tone.Synth(Options.triangleOptions).toDestination()
+const noiseSynth    = new Tone.NoiseSynth().toDestination()
 
 let song = {}
-var pulsePart    = new Tone.Part()
-var squarePart   = new Tone.Part()
-var trianglePart = new Tone.Part()
-var noisePart    = new Tone.Part()
+let pulsePart    = new Tone.Part()
+let squarePart   = new Tone.Part()
+let trianglePart = new Tone.Part()
+let noisePart    = new Tone.Part()
 
 const button = document.querySelector("button")
 
 function setSong() {
 
-    song = songs[0]
+    song = songs[1]
 
-    pulsePart.removeAll()
-    squarePart.removeAll()
-    trianglePart.removeAll()
-    noisePart.removeAll()
+    pulsePart.clear()
+    squarePart.clear()
+    trianglePart.clear()
+    noisePart.clear()
 
     if (song.pulse != null) {
         pulsePart = new Tone.Part((time, note) => {
-            pulseSynth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
+            pulseSynth.triggerAttackRelease(note.name, note.duration, now + time, note.velocity)
         }, song.pulse)
       }
       
       if(song.square != null) {
         squarePart = new Tone.Part((time, note) => {
-            squareSynth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
+            squareSynth.triggerAttackRelease(note.name, note.duration, now + time, note.velocity)
         }, song.square)
       }
       
       if(song.triangle != null) {
         trianglePart = new Tone.Part((time, note) => {
-            triangleSynth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
+            triangleSynth.triggerAttackRelease(note.name, note.duration, now + time, note.velocity)
         }, song.triangle)
       }
       
       if(song.noise != null) {
         noisePart = new Tone.Part((time, note) => {
-            noiseSynth.triggerAttackRelease(note.duration, time, note.velocity)
+            noiseSynth.triggerAttackRelease(note.duration, now + time, note.velocity)
         }, song.noise)
       }
 }
@@ -66,6 +66,7 @@ function stop() {
     if(song.noise != null)    noisePart.stop(0)
 }
 
+const now = Tone.now()
 setSong()
 
 button.addEventListener('click', () => {
@@ -83,10 +84,7 @@ button.addEventListener('click', () => {
 
 Tone.Transport.on('stop', () => { button.textContent = 'Restart' })
 
-/*
 button.addEventListener("click", async () => {
     await Tone.start()
-    const osc = new Tone.FMOscillator("Bb4").toDestination().start()
-    osc.frequency.rampTo("D2", 3)
+    console.log('audio is ready')
 })
-*/
